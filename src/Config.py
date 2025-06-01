eaf02512cbcf5f9ee7a2f4894981ce3b7777805f
noritsune <bill.gatess4037@gmail.com>
2025-06-01 21:19:01 +0900

    Revert "足踏みを高速化"
    This reverts commit 1eba04fc3658e89abb16ed01ff24557cb2336e36.

--- src/Config.py
+++ src/Config.py
@@ -54,9 +54,9 @@ class Configuration:
         self.ps4_torot_color = PS4_TOROT_COLOR
 
         #################### COMMANDS ####################
-        self.max_x_velocity = 0.26 * 4
-        self.max_y_velocity = 0.16 * 4
-        self.max_yaw_rate = 5
+        self.max_x_velocity = 0.26
+        self.max_y_velocity = 0.16
+        self.max_yaw_rate = 1.1
         self.max_pitch = 25.0 * np.pi / 180.0
 
         #################### MOVEMENT PARAMS ####################
@@ -81,7 +81,7 @@ class Configuration:
 
         #################### SWING ######################
         self.z_coeffs = None
-        self.z_clearance = 0.08
+        self.z_clearance = 0.16
         self.alpha = (
             0.5  # Ratio between touchdown distance and total horizontal stance movement
         )
@@ -97,10 +97,10 @@ class Configuration:
             [[1, 1, 1, 0], [1, 0, 1, 1], [1, 0, 1, 1], [1, 1, 1, 0]]
         )
         self.overlap_time = (
-            0.0  # duration of the phase where all four feet are on the ground
+            0.10  # duration of the phase where all four feet are on the ground
         )
         self.swing_time = (
-            0.13  # duration of the phase when only two feet are on the ground 
+            0.06  # duration of the phase when only two feet are on the ground 
         )
 
         ######################## GEOMETRY ######################
